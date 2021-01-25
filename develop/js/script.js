c398fbd26ca369533e9e45870dba6e363632f7df
jennifer gomez <jennifer23gomez7@gmail.com>
2021-01-24 19:57:07 -0800

    timeblocks added

---
+++ develop/js/script.js
@@ -0,0 +1,6 @@
+// create a grid for the schedule of current day
+// container block for each hour of the day
+//color code blocks by past, present, future.
+//event listener for time block, save
+//render saved to local storage
+
